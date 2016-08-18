d20b930283045b96a0dd817d5a61ea9be9994eef
blhack <ryan@blhackbook-pro-2.local>
2016-08-17 20:29:11 -0700

    More buttons supported

--- heliosSender.ino
+++ heliosSender.ino
@@ -1,24 +1,27 @@
 //Sender Code
 
 #define numberOfTorches 8
+#define numberOfButtons 19
 
 char str[4];
 int chaseSpeed = 0;
 int chaseNumber = 0;
 unsigned long nextChase = 0;
-int allTorchesPin = 11;
+int allTorchesPin = 21;
 byte someByte;
 int keepAlive = 0;
 
 
 void setup() {
-  for(int i=2; i<2+numberOfTorches; i++) {
+  //i = 2 because of RX and TX pins.
+  //set all of the button pins to INPUT_PULLUP
+  //buttons are connected to the arduino sequentially to make this easier.
+  for(int i=2; i<2+numberOfButtons; i++) {
     pinMode(i, INPUT_PULLUP);
   }
+  
   pinMode(allTorchesPin, INPUT_PULLUP);
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
 }
 
 void sendData(int someValue) {
@@ -27,17 +30,22 @@ void sendData(int someValue) {
 }
 
 void loop() {
-  for(int i=2; i<2+numberOfTorches; i++) {
+  for(int i=2; i<2+numberOfButtons; i++) {
     if (digitalRead(i) == LOW) {
       sendData(i - 2);
     }
   }
-  if (analogRead(0) > 10) {
-    chaseSpeed = map(analogRead(0), 0, 1024, 1024, 100);
-    //sendData(chaseNumber);
+  if (analogRead(1) < 1013) {
+    chaseSpeed = map(analogRead(1), 1024, 0, 800, 100);
     if (millis() > nextChase) {
-      chaseNumber++;
+      if (chaseSpeed == 100) {
+        chaseNumber = random(0,8);
+      }
+      else {
+        chaseNumber++;
+      }
       nextChase = millis() + chaseSpeed;
+      sendData(chaseNumber);
     }
     if (chaseNumber >= numberOfTorches) {
       chaseNumber = 0;
